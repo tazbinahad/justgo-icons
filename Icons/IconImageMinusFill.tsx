c3d96493bdafaf6da9327ec7931a4c28affdb5cb
Sayful Islam Shakib <connect.sayful@gmail.com>
2026-01-13 11:47:35 +0600

    i series all icons added.

---
+++ Icons/IconImageMinusFill.tsx
@@ -0,0 +1,21 @@
+import WithIconProps, { IconBaseProps } from "./_base/WithIconProps";
+
+const IconImageMinusFill = (props: IconBaseProps) => {
+  return WithIconProps(props, ({ width, height, fill, className }) => {
+    return (
+      <svg
+        viewBox="0 0 24 24"
+        width={width}
+        height={height}
+        fill={fill || "currentColor"}
+        xmlns="http://www.w3.org/2000/svg"
+        className={className}
+      >
+        <path
+          d="M16 18C15.4477 18 15 18.4477 15 19C15 19.5523 15.4477 20 16 20H22C22.5523 20 23 19.5523 23 19C23 18.4477 22.5523 18 22 18H16ZM13.3 21H5C3.9 21 3 20.1 3 19V5C3 3.9 3.9 3 5 3H19C20.1 3 21 3.9 21 5V13.3C20.4 13.1 19.7 13 19 13C17.9 13 16.8 13.3 15.9 13.9L14.8918 12.5317C14.6951 12.2648 14.298 12.2597 14.0945 12.5213L11.3812 16.0098C11.1853 16.2617 10.8068 16.2681 10.6025 16.023L8.89754 13.977C8.69324 13.7319 8.31468 13.7383 8.11875 13.9902L5.62764 17.193C5.3722 17.5215 5.60625 18 6.02232 18H13.1C13 18.3 13 18.7 13 19C13 19.7 13.1 20.4 13.3 21Z"
+        />
+      </svg>
+    );
+  });
+};
+export default IconImageMinusFill;
