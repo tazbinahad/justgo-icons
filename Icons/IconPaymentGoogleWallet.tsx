1639160569bf6a43820c1452249f4811e91e4e19
Sayful Islam Shakib <connect.sayful@gmail.com>
2026-01-13 12:55:22 +0600

    Bluk, Duotone and color issues issues fixed.

--- Icons/IconPaymentGoogleWallet.tsx
+++ Icons/IconPaymentGoogleWallet.tsx
@@ -7,7 +7,7 @@ const IconPaymentGoogleWallet = (props: IconBaseProps) => {
         viewBox="0 0 24 24"
         width={width}
         height={height}
-        fill={fill || "currentColor"}
+        fill="none"
         xmlns="http://www.w3.org/2000/svg"
         className={className}
       >
@@ -17,21 +17,25 @@ const IconPaymentGoogleWallet = (props: IconBaseProps) => {
             fillRule="evenodd"
             clipRule="evenodd"
             d="M24 8.98404V4.98358C24 2.67362 22.287 0.791016 20.1852 0.791016H3.81486C1.71197 0.812052 -0.000976562 2.69465 -0.000976562 4.98358V8.98399C-0.000976562 9.39032 0.291369 9.71165 0.661131 9.71165H23.3389C23.7086 9.71165 24 9.39032 24 8.98399V8.98404Z"
+            fill="#34A853"
           />
           <path
             fillRule="evenodd"
             clipRule="evenodd"
             d="M24 12.4924V8.51298C24 6.20194 22.287 4.31934 20.1852 4.31934H3.81486C1.71197 4.31934 -0.000976562 6.20194 -0.000976562 8.51298V12.5134C-0.000976562 12.9197 0.291369 13.2411 0.661131 13.2411H23.3389C23.7086 13.2189 24 12.8987 24 12.4924Z"
+            fill="#FBBC04"
           />
           <path
             fillRule="evenodd"
             clipRule="evenodd"
             d="M24 16.0222V12.0218C24 9.71077 22.287 7.82812 20.1852 7.82812H3.81486C1.71197 7.82813 -0.000976562 9.71077 -0.000976562 12.0218V16.0222C-0.000976562 16.4286 0.291369 16.7488 0.661131 16.7488H23.3389C23.7086 16.7278 24 16.4065 24 16.0222Z"
+            fill="#EA4335"
           />
           <path
             fillRule="evenodd"
             clipRule="evenodd"
             d="M15.2602 14.0967L-0.000976562 10.2033V19.0169C-0.000976562 21.3279 1.71201 23.2106 3.81481 23.2106H20.1852C22.287 23.2106 24 21.3279 24 19.0169V10.0322L20.36 12.9417C18.8811 14.1177 17.0315 14.5461 15.2602 14.0967V14.0967Z"
+            fill="#4285F4"
           />
         </g>
         <defs>
@@ -39,6 +43,7 @@ const IconPaymentGoogleWallet = (props: IconBaseProps) => {
             <rect
               width="24"
               height="24"
+              fill="white"
             />
           </clipPath>
         </defs>
--- Icons/IconPowerbiStyleColor.tsx
+++ Icons/IconPowerbiStyleColor.tsx
@@ -7,18 +7,21 @@ const IconPowerbiStyleColor = (props: IconBaseProps) => {
         viewBox="0 0 24 24"
         width={width}
         height={height}
-        fill={fill || "currentColor"}
+        fill="none"
         xmlns="http://www.w3.org/2000/svg"
         className={className}
       >
         <path
           d="M13.5 22H18.5C19.0525 22 19.5 21.5525 19.5 21V3C19.5 2.4475 19.0525 2 18.5 2H13.5C12.9475 2 12.5 2.4475 12.5 3V21C12.5 21.5525 12.9475 22 13.5 22Z"
+          fill="url(#paint0_linear_3420_808)"
         />
         <path
           d="M9.5 22H14.5C15.0525 22 15.5 21.5525 15.5 21V8C15.5 7.4475 15.0525 7 14.5 7H9.5C8.9475 7 8.5 7.4475 8.5 8V21C8.5 21.5525 8.9475 22 9.5 22Z"
+          fill="url(#paint1_linear_3420_808)"
         />
         <path
           d="M5.5 22H10.5C11.0525 22 11.5 21.5525 11.5 21V13C11.5 12.4475 11.0525 12 10.5 12H5.5C4.9475 12 4.5 12.4475 4.5 13V21C4.5 21.5525 4.9475 22 5.5 22Z"
+          fill="url(#paint2_linear_3420_808)"
         />
         <defs>
           <linearGradient
